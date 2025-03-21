255af147a3e51cb19b8ca106d20f09a074ee5ae0
Suhail Khan <122381310+suhailskhan9@users.noreply.github.com>
2025-03-21 16:03:38 +0530

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,19 +1,15 @@
 from pydantic import BaseModel, EmailStr
 
-# Signup schema
 class UserCreate(BaseModel):
     full_name: str  
     email: EmailStr
     password: str
 
-# Login schema 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
-# Task creation schema
 class TaskCreate(BaseModel):
-    created_by: str
     title: str
     description: str
     status: str
