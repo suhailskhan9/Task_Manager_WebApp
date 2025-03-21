7af93a100842b72b6717b6c429aec3623b2b4e40
Suhail Khan <122381310+suhailskhan9@users.noreply.github.com>
2025-03-21 05:54:35 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://task-manager-web-app-lilac.vercel.app/"], 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],  
